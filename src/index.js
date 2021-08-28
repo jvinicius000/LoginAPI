2acb959253ecb7093df0da9eb3c554be9fdb0620
KallelGaNewk <kallelgn@gmail.com>
2021-08-28 00:42:44 -0300

    Hardle 404 correct

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use(function(req, res, next) {
+    res.removeHeader("X-Powered-By");
+    next();
+});
+
 app.get('/', (req, res)=> {
     res.json({name: "JVinicius API", version: "1.0.0"});
 });
@@ -15,10 +20,7 @@ require('./controllers')(app);
 
 // Handle 404
 app.use(function(req, res, next) {
-    res.removeHeader("X-Powered-By");
-    if(res.status(404)) {
-        return res.status(404).send({success:false, error: `Controller not found!`});
-    }
+    res.status(404).send({success:false, error: `Controller not found!`});
 });
 
 app.listen(process.env.PORT, (err)=>{
